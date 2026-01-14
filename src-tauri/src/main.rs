9ccd6f4450f89af9d81de68a15e273b8a4dd5c3d
Little-Red-Cap <little-red-cap@foxmail.com>
2026-01-14 20:53:20 +0800

    Update main.rs

--- src-tauri/src/main.rs
+++ src-tauri/src/main.rs
@@ -7,15 +7,15 @@ mod settings;
 mod system_fonts;
 
 fn main() {
-  tauri::Builder::default()
-      .plugin(tauri_plugin_dialog::init())
-      .invoke_handler(tauri::generate_handler![
-        settings::save_settings,
-        settings::load_settings,
-        font_pipeline::generate_font,
-        font_pipeline::export_font,
-        system_fonts::list_system_fonts
-      ])
-      .run(tauri::generate_context!())
-      .expect("Failed to run Tauri application");
+    tauri::Builder::default()
+        .plugin(tauri_plugin_dialog::init())
+        .invoke_handler(tauri::generate_handler![
+            settings::save_settings,
+            settings::load_settings,
+            font_pipeline::generate_font,
+            font_pipeline::export_font,
+            system_fonts::list_system_fonts
+        ])
+        .run(tauri::generate_context!())
+        .expect("Failed to run Tauri application");
 }
